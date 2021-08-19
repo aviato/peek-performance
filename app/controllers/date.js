5d0c8687e98af38d5db1e062bc9b6970dc8714a5
Ryan Price <rsprice90@gmail.com>
2021-08-19 00:01:24 -0700

    feat: add basic calendar day view
    * Shows activities
    * Naive implementation

--- app/controllers/date.js
+++ app/controllers/date.js
@@ -1,3 +1,8 @@
 import Controller from '@ember/controller';
 
-export default class DateController extends Controller {}
+export default class DateController extends Controller {
+  hours = Array.from({length: 24 }, (_x, i) => ({
+      formattedTime: `${i < 10 ? 0 : ''}${i}:00`,
+      hourNumber: i
+  }));
+}
